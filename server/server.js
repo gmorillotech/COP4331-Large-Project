7df9ce906cde05bd81f2b416dc5aae127c183291
Bhagyesh Jethwani <jethwani.bhagyesh@gmail.com>
2026-03-16 01:15:52 -0400

    changed port to 5000

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ app.get("/api/health", (req, res) => {
 app.use("/api", authRoutes);
 app.use("/api", cardRoutes);
 
-const PORT = process.env.PORT || 5050;
+const PORT = process.env.PORT || 5000;
 
 const startServer = async () => {
   try {
