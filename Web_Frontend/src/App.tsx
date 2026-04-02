09f9e0a1e7d3a56111a401266a54a5cbe592a6b9
Ava Sanford <avasanford4@gmail.com>
2026-04-01 21:15:52 -0400

    Login, Register, Email Verificaion, Password Reset, and new API
    the new API is for resending email verification for already existing users

--- Web_Frontend/src/App.tsx
+++ Web_Frontend/src/App.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import CardPage from './pages/CardPage.tsx';
 import LoginPage from './pages/LoginPage.tsx';
+import VerifyEmail from './VerifyEmail';
+import ResetPassword from './ResetPassword';
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/cards" element={<CardPage />} />
+        <Route path="/verify" element={<VerifyEmail />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
       </Routes>
     </BrowserRouter>
   );
