4e9be8f0a9a3ef2dc65239c6b43e861f5f22e779
Brian Huang <brianh+git@jswebinc.com>
2026-04-02 22:20:58 -0400

    Branch cleanup
    Deleted old client folder

--- client/src/Path.ts
+++
@@ -1,9 +0,0 @@
-const app_name = "167.71.81.89"; // eventually change to domain
-
-export function buildPath(route: string): string {
-  if (import.meta.env.MODE !== "development") {
-    return `http://${app_name}:5000/${route}`;
-  }
-
-  return `http://127.0.0.1:5000/${route}`;
-}
