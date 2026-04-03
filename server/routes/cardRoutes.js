981ac86e3451f8fb9c4322bda87d6ef060487122
Bhagyesh Jethwani <jethwani.bhagyesh@gmail.com>
2026-04-03 01:38:06 -0400

    google api might need to add the ip and port as authorized referrers

--- server/routes/cardRoutes.js
+++
@@ -1,9 +0,0 @@
-
-const express = require("express");
-const router = express.Router();
-const { addCard, searchCards } = require("../controllers/cardController");
-
-router.post("/addCard", addCard);
-router.post("/searchCards", searchCards);
-
-module.exports = router;
