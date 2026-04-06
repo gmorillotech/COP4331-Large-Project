91e595c6c7414a260e78a64b8a61e91dced41c2d
Brian Huang <brianh+git@jswebinc.com>
2026-04-06 13:31:56 -0400

    Add admin shell: layout, navigation, route guards, stub pages

---
+++ Web_Frontend/src/pages/admin/AdminSearchPage.tsx
@@ -0,0 +1,10 @@
+function AdminSearchPage() {
+  return (
+    <div>
+      <h1>Admin Search</h1>
+      <p>Search for locations and view active reports.</p>
+    </div>
+  );
+}
+
+export default AdminSearchPage;
