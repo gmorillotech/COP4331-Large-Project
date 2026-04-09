ceaac806bf62eca644ac4d0e61dd745aec765077
Brian Huang <brianh+git@jswebinc.com>
2026-04-09 01:13:50 -0400

    base-url-removal
    forces api to match server; prevents CORS errors

--- Web_Frontend/src/config/active.ts
+++ Web_Frontend/src/config/active.ts
@@ -4,4 +4,4 @@ import { API_BASE_URL as LIVE_API_BASE_URL } from './live';
 // Flip this to false when you want the web frontend to talk to your local backend.
 const USE_LIVE_API = true;
 
-export const API_BASE_URL = USE_LIVE_API ? LIVE_API_BASE_URL : LOCAL_API_BASE_URL;
+export const API_BASE_URL = '';
