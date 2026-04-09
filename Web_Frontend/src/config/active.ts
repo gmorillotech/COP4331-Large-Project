662993d50e113cbd2ca7be859763f7b8476fe120
Brian Huang <brianh+git@jswebinc.com>
2026-04-09 01:25:39 -0400

    corrected active.ts

--- Web_Frontend/src/config/active.ts
+++ Web_Frontend/src/config/active.ts
@@ -1,7 +1,1 @@
-import { API_BASE_URL as LOCAL_API_BASE_URL } from './local';
-import { API_BASE_URL as LIVE_API_BASE_URL } from './live';
-
-// Flip this to false when you want the web frontend to talk to your local backend.
-const USE_LIVE_API = true;
-
 export const API_BASE_URL = '';
